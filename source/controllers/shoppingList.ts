2a90dd5fd566c789cde32a716074241dfc84079c
Dan Örjestad <danorjest@gmail.com>
2021-01-28 13:06:47 +0100

    Moving to async/await from promises

--- source/controllers/shoppingList.ts
+++ source/controllers/shoppingList.ts
@@ -1,8 +1,8 @@
 import { Request, Response } from 'express';
 import mongoose from 'mongoose';
 import logging from '../config/logging';
-import ShoppingList from '../models/shoppingList';
 import IShoppingList from '../interfaces/shoppingList';
+import ShoppingList from '../models/shoppingList';
 const workspace = 'ListController';
 
 /* Used to select just the relevant fields from the DB */
@@ -27,63 +27,70 @@ const createList = (req: Request, res: Response) => {
         items
     });
 
-    return list
-        .save()
-        .then((result) => {
-            logging.info(workspace, 'Created list.');
-            return res.status(201).json({
-                ShoppingList: result
-            });
-        })
-        .catch((error) => {
-            return res.status(500).json({
-                message: error.message,
-                error
-            });
+    try {
+        let result = list.save();
+        logging.info(workspace, 'Created list.');
+        res.status(201).json({
+            list: result
         });
+    } catch (err) {
+        res.status(500).json();
+        logging.error(workspace, `Could not save list ${list}.`, err);
+    }
 };
 
 // TODO: hide list instead of delete?
 // delete('/delete/list:id')
 const deleteList = async (req: Request, res: Response) => {
-    let document = await ShoppingList.findByIdAndDelete({ _id: req.params.id });
-    if (document)
-        res.status(200).json({ message: 'List deleted.' });
-    else
-        res.status(400).json({ message: 'List not found.' });
+    let id = req.params.id;
+    try {
+        // findByIdAndDelete returns the list if successfull
+        // or returns null if unsuccessfull
+        let list = await ShoppingList.findByIdAndDelete({ _id: id });
+        if (list) {
+            logging.info(workspace, `List with id ${id} deleted`);
+            res.status(200).json({ message: 'List deleted.' });
+        } else {
+            logging.info(workspace, `List with id ${id} unsucessfully deleted.`, list);
+            res.status(400).json({ message: 'List not found.' });
+        }
+    } catch (err) {
+        res.status(500);
+        logging.error(workspace, 'Could not delete list.', err);
+    }
 };
 
 // get('/get/lists')
-const getAllLists = (req: Request, res: Response) => {
-    ShoppingList.find()
-        .select(selection)
-        .exec()
-        .then((lists: any) => {
+const getAllLists = async (req: Request, res: Response) => {
+    try {
+        let lists = await ShoppingList.find().select(selection);
+        if (lists) {
             logging.info(workspace, 'Sent all lists.');
-            return res.status(200).json({
+            res.status(200).json({
                 lists: lists,
                 count: lists.length
             });
-        })
-        .catch((error: any) => {
-            return res.status(500).json({
-                message: error.message,
-                error
-            });
-        });
+        } else {
+            logging.error(workspace, 'Could not get lists.');
+            res.status(400);
+        }
+    } catch (err) {
+        res.status(500);
+    }
 };
 
 // TODO: Support multiple indexes in one request
 // delete('/update/list/deleteitem/:id')
-const deleteItemFromList = (req: Request, res: Response) => {
+const deleteItemFromList = async (req: Request, res: Response) => {
     let errorMsg = '';
     if (!req.params.id) errorMsg += 'No ID specified. ';
     if (req.body.index === undefined) errorMsg += 'No index specified. ';
     else if (req.body.index < 0) errorMsg += 'Index must be >= 0. ';
     if (errorMsg !== '') return res.status(400).json({ message: errorMsg });
 
-    ShoppingList.findById({ _id: req.params.id })
-        .then((document:IShoppingList) => {
+    try {
+        let document: IShoppingList | null = await ShoppingList.findById({ _id: req.params.id });
+        if (document !== null) {
             let index = req.body.index;
             if (!document.items || index > document.items.length) {
                 return res.status(400).json({ message: 'Item not found.' });
@@ -93,11 +100,13 @@ const deleteItemFromList = (req: Request, res: Response) => {
             document.markModified('items');
             document.save();
             res.status(200).json(document);
-        })
-        .catch((ex:any) => {
-            logging.error(workspace, 'Could not add item to list', ex);
-            return res.status(400).json({ message: 'Could not add item.' });
-        });
+        } else {
+            res.status(400).json({ message: 'List not found.' });
+        }
+    } catch (ex: any) {
+        logging.error(workspace, 'Could not add item to list', ex);
+        return res.status(400).json({ message: 'Could not add item.' });
+    }
 };
 
 // TODO: support multiple items in one request
@@ -111,22 +120,24 @@ const updateItem = (req: Request, res: Response) => {
     if (req.body.index === undefined) errorMsg += 'No item index specified ';
     if (errorMsg !== '') return res.status(400).json({ message: errorMsg });
 
+    ShoppingList.findById({ _id: req.params.id })
+        .then((document: IShoppingList) => {
+            let index = Number.parseInt(req.body.index);
+            if (!document.items || isNaN(req.body.index) || index > document.items.length) {
+                throw new Error('Item not found.');
+            }
 
-    ShoppingList.findById({ _id: req.params.id }).then((document:IShoppingList) => {
-        let index = Number.parseInt(req.body.index);
-        if (!document.items || isNaN(req.body.index) || index > document.items.length) {
-            throw new Error("Item not found.");
-        }
-
-        document.items[index] = req.body.items;
-        document.markModified('items');
-        return document.save();
-    }).then((document: IShoppingList) => {
-        res.status(200).json(document);
-    }).catch((ex: Error) => {
-        res.status(400).json({ message: ex });
-        logging.error(workspace, 'Error updating item.', ex);
-    });
+            document.items[index] = req.body.items;
+            document.markModified('items');
+            return document.save();
+        })
+        .then((document: IShoppingList) => {
+            res.status(200).json(document);
+        })
+        .catch((ex: Error) => {
+            res.status(400).json({ message: ex });
+            logging.error(workspace, 'Error updating item.', ex);
+        });
 };
 
 // put('/update/list/additem/:id')
