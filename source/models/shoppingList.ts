947938484549a4bda28238a31aba6ad82ef39c17
Dan Forsberg <dan.s.forsberg@gmail.com>
2021-01-29 08:46:59 +0100

    Added Schema for ListItems

--- source/models/shoppingList.ts
+++ source/models/shoppingList.ts
@@ -2,10 +2,17 @@ import mongoose, { Schema } from 'mongoose';
 import logging from '../config/logging';
 import IShoppingList from '../interfaces/shoppingList';
 
+const ShoppingListItemSchema: Schema = new Schema({
+    item: { type: String, required: true, trim: true },
+    bought: { type: Boolean, required: true, default: false },
+    amount: { type: Number, required: false },
+    price: { type: Number, required: false }
+});
+
 const ShoppingListSchema: Schema = new Schema(
     {
-        name: { type: String, required: false },
-        items: { type: [Object], required: false }
+        name: { type: String, required: false, trim: true },
+        items: { type: [ShoppingListItemSchema], required: false }
     },
     {
         timestamps: true
