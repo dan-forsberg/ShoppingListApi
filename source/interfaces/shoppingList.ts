0519881d56a697b72d39a0f2b0399a3ddd2c2918
Dan Forsberg <dan.s.forsberg@gmail.com>
2021-01-29 09:08:46 +0100

    add _id to Item, hidden to List

--- source/interfaces/shoppingList.ts
+++ source/interfaces/shoppingList.ts
@@ -1,13 +1,15 @@
 import { Document } from 'mongoose';
-export default interface IShoppingListItem {
+export default interface IShoppingListItem extends Document {
     item: String;
     bought: Boolean;
     cost?: Number;
     amount?: Number;
+    _id: Number;
 }
 
 export default interface IShoppingList extends Document {
     name?: String;
     items?: Array<IShoppingListItem>;
     _id: Number;
+    hidden: Boolean;
 }
