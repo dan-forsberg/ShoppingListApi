43e33742d4eeeb388b4a03df6b6184dd87223032
Dan Forsberg <dan.s.forsberg@gmail.com>
2021-01-30 08:46:05 +0100

    Made items required, as it is in the schema

--- source/interfaces/shoppingList.ts
+++ source/interfaces/shoppingList.ts
@@ -3,7 +3,7 @@ import IShoppingListItemSchema from '../models/shoppingList';
 
 export default interface IShoppingList extends Document {
     name?: String;
-    items?: Array<typeof IShoppingListItemSchema>;
+    items: Array<typeof IShoppingListItemSchema>;
     _id: Number;
     hidden: Boolean;
 }
