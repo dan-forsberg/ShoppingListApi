3657ff64876c62c68100cbd8d12d2027058f3a8e
Dan Forsberg <dan.s.forsberg@gmail.com>
2021-01-30 09:08:06 +0100

    fixed route for toggleBought

--- source/routes/shoppingLists.ts
+++ source/routes/shoppingLists.ts
@@ -2,11 +2,16 @@ import express from 'express';
 import controller from '../controllers/shoppingList';
 
 const router = express.Router();
+
 router.post('/create/list', controller.createList);
+
 router.get('/get/lists', controller.getAllLists);
+
 router.put('/update/list/addItem/:id', controller.addItemsToList);
+
 router.patch('/update/list/updateItem/:id', controller.updateItem);
-router.patch('/update/list/toggleBought/:id')
+router.patch('/update/list/toggleBought/:id', controller.toggleItemAsBought);
+
 router.delete('/update/list/deleteItem/:id', controller.deleteItemFromList);
 router.delete('/delete/list/:id', controller.deleteList);
 export = router;
