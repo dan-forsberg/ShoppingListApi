d93b767b741e99659c0da7c7f95bea26ad8f4c9e
Dan Forsberg <dan.s.forsberg@gmail.com>
2021-01-31 08:21:21 +0100

    rewrote controller

--- source/controllers/shoppingList.ts
+++ source/controllers/shoppingList.ts
@@ -1,28 +1,32 @@
-import { Request, Response } from 'express';
-import logging from '../config/logging';
-import IShoppingList from '../interfaces/shoppingList';
-import ShoppingList from '../models/shoppingList';
+import IShoppingList from "../interfaces/shoppingList";
+import logging from "../config/logging";
+import ShoppingList from "../models/shoppingList";
+import { ListNotFoundError, ListItemNotFoundError } from "../interfaces/errors";
+import { Request, Response } from "express";
+import mongoose from "mongoose";
+const workspace = "ListController";
+const selection = "_id name createdAt items";
 
-const workspace = 'ListController';
-
-/* Used to select just the relevant fields from the DB */
-const selection = '_id name createdAt items';
-//TODO: Add more proper error handling, return the results more consistently
-//TODO: Only return params in selection is returned
-//TODO: Use errorhandler middleware
-
-function checkProperties(req: Request, properties: String[]): String | null {
-    let errorMsg = "Missing property/-ies: ";
-    let error = false;
-    properties.forEach(prop => {
-        if (!req.body.hasOwnProperty(prop)) {
-            errorMsg += `${prop} `;
-            error = true;
-        }
-    });
-
-    return (error ? errorMsg : null);
+type ListListItem = {
+    /* any because @types/mongoose isn't properly typed */
+    shoppingList: any | null;
+    listItem: any | null;
 };
+
+async function getListItem(listID: any, itemID: any): Promise<ListListItem> {
+    let result: ListListItem = { shoppingList: null, listItem: null };
+    /* any because @types/mongoose isn't typed properly
+       or because I can't figure out how to type things properly to get
+       TS to be happy. This is actually a IShoppingList (which contains IShoppingListItem) */
+    let shoppingList: any = await ShoppingList.findById({ _id: listID });
+
+    if (shoppingList !== null) {
+        result.shoppingList = shoppingList;
+        result.listItem = shoppingList.items.id(itemID);
+    }
+
+    return result;
+}
 
 function makeStringToItem(item: any): { item: string, bought: boolean; } {
     if (typeof item === 'string')
@@ -32,166 +36,154 @@ function makeStringToItem(item: any): { item: string, bought: boolean; } {
     return item;
 };
 
-type ListListItem = {
-    /* any because @types/mongoose isn't properly typed */
-    shoppingList: any | null,
-    listItem: any | null,
+const getAllLists = async (req: Request, res: Response) => {
+    try {
+        const lists: Array<IShoppingList> = await ShoppingList.find().where('hidden').equals(false).select(selection);
+        res.status(200).json({ 'lists': lists, count: lists.length });
+    } catch (ex) {
+        logging.error(workspace, "Could not get lists.", ex);
+        res.status(500);
+    }
 };
 
-async function getListItem(listID: string, itemID: string): Promise<ListListItem> {
-    let result: ListListItem = { shoppingList: null, listItem: null };
-    let shoppingList: IShoppingList | null =
-        await ShoppingList.findById(
-            { _id: listID }
-        );
+const hideList = async (req: Request, res: Response) => {
+    const listID = req.params.listID;
+    try {
+        if (listID === undefined) {
+            throw new ListNotFoundError("List not found.");
+        }
 
-    if (shoppingList !== null) {
-        result.shoppingList = shoppingList;
-        //@ts-ignore -- I promise this is right, I(?) have just not type:d thing right
-        result.listItem = shoppingList.items.id(req.body.item);
+        const shoppingList = await ShoppingList.findById({ _id: listID });
+        if (!shoppingList) {
+            throw new ListNotFoundError("List not found.");
+        } else {
+            shoppingList.hidden = true;
+            await shoppingList.save();
+            res.status(200).json({ list: shoppingList });
+        }
+    } catch (ex) {
+        if (ex instanceof ListNotFoundError) {
+            res.status(400).json({ message: `List not found, ID: ${listID}` });
+        } else {
+            logging.error(workspace, "Could not hide list.", ex.message);
+            res.status(500).json({ message: `Could not hide list with ID: ${listID}` });
+        }
     }
+};
 
-    return result;
-}
-
-// post('/create/list')
-const createList = async (req: Request, res: Response) => {
-    let errorMsg = checkProperties(req, ["items"]);
-    if (errorMsg) {
-        res.status(401).json({ 'message': errorMsg });
-        return;
+const deleteItemFromList = async (req: Request, res: Response) => {
+    const { listID, itemID } = req.params;
+    const { shoppingList, listItem } = await getListItem(listID, itemID);
+    try {
+        if (!shoppingList) {
+            throw new ListNotFoundError(`List not found, ID: ${listID}`);
+        } else if (!listItem) {
+            throw new ListItemNotFoundError(`List item not found, ID: ${itemID}`);
+        } else {
+            listItem.remove();
+            await shoppingList.save();
+            res.status(200).json({ list: shoppingList });
+        }
+    } catch (ex) {
+        if (ex instanceof ListNotFoundError) {
+            res.status(400).json({ message: ex.message });
+        } else if (ex instanceof ListItemNotFoundError) {
+            res.status(400).json({ message: ex.message });
+        } else {
+            logging.error(workspace, "Could not delete item from list.", ex.message);
+            res.status(500);
+        }
     }
+};
 
-    let { name, items } = req.body;
-    items = items.map(makeStringToItem);
+const updateItem = async (req: Request, res: Response) => {
+    const { listID, itemID } = req.params;
+    const { shoppingList, listItem } = await getListItem(listID, itemID);
+    const newItem = makeStringToItem(req.body.item);
+    try {
+        if (!shoppingList) {
+            throw new ListNotFoundError(`List not found, ID: ${listID}`);
+        } else if (!listItem) {
+            throw new ListItemNotFoundError(`List item not found, ID: ${itemID}`);
+        } else {
+            Object.assign(listItem, newItem);
+            shoppingList.markModified('items');
+            await shoppingList.save();
+            res.status(202).json({ list: shoppingList });
+        }
+    } catch (ex) {
+        if (ex instanceof ListNotFoundError) {
+            res.status(400).json({ message: ex.message });
+        } else if (ex instanceof ListItemNotFoundError) {
+            res.status(400).json({ message: ex.message });
+        } else {
+            logging.error(workspace, "Could not update item.", ex.message);
+            res.status(500);
+        }
+    }
+};
 
-    const list = new ShoppingList({
-        name,
-        items
-    });
+const addItemsToList = async (req: Request, res: Response) => {
+    const listID = req.params.listID;
+    const newItems = req.body.items.map(makeStringToItem);
+    const shoppingList = await ShoppingList.findById({ _id: listID });
 
     try {
-        await list.save();
-        res.status(201).json({
-            list: list /* TODO: fix, return a proper list not just [] */
-        });
-    } catch (error) {
-        res.status(401).json({ message: error.message });
-        logging.error(workspace, `Could not save list ${list}.`);
+        if (!shoppingList) {
+            throw new ListNotFoundError(`Could not find list with ID: ${listID}`);
+        } else {
+            shoppingList.items.push(...newItems);
+            await shoppingList.save();
+            res.status(200).json(shoppingList);
+        }
+    } catch (ex) {
+        if (ex instanceof ListNotFoundError) {
+            res.status(400).json({ message: ex.message });
+        } else {
+            logging.error(workspace, "Could not add items to list.", ex.message);
+            res.status(500);
+        }
     }
 };
 
-// delete('/delete/list:id')
-const hideList = async (req: Request, res: Response) => {
-    let errorMsg = checkProperties(req, ["id"]);
-    if (errorMsg) {
-        res.status(401).json({ 'message': errorMsg });
-        return;
-    }
-
-    let id = req.params.id;
-    let shoppingList: IShoppingList = await ShoppingList.findById({ _id: id });
-    if (shoppingList !== null) {
-        shoppingList.hidden = true;
-        await shoppingList.save();
-        res.status(200).json({ message: 'List deleted.' });
-    } else {
-        res.status(400).json({ message: 'List not found.' });
-    }
-};
-
-// get('/get/lists')
-const getAllLists = async (req: Request, res: Response) => {
-    let shoppingLists: Array<IShoppingList> = await ShoppingList
-        .where('hidden')
-        .equals(false)
-        .select(selection);
-
-    if (shoppingLists) {
-        res.status(200).json(shoppingLists);
-    } else {
-        logging.error(workspace, 'Could not get lists.');
-        res.status(400);
-    }
-};
-
-// TODO: Support multiple indexes in one request
-// delete('/update/list/deleteitem/:id')
-const deleteItemFromList = async (req: Request, res: Response) => {
-    let errorMsg = checkProperties(req, ["item"]);
-    if (errorMsg) {
-        res.status(401).json({ 'message': errorMsg });
-        return;
-    }
-
-    let { shoppingList, listItem } = await getListItem(req.params.id, req.body.item);
-    if (!shoppingList) {
-        res.status(400).json({ message: 'List not found.' });
-    } else if (!listItem) {
-        res.status(400).json({ message: 'Item not found' });
-    } else {
-        listItem.remove();
-        await shoppingList.save();
-        res.status(200).json({ message: 'Item deleted.' });
-    }
-};
-
-// TODO: support multiple items in one request
-// patch('/update/list/updateitem/:id')
-const updateItem = async (req: Request, res: Response) => {
-    let errorMsg = checkProperties(req, ["item", "item.id"]);
-    if (errorMsg) {
-        res.status(401).json({ 'message': errorMsg });
-        return;
-    }
-
-    let { shoppingList, listItem } = await getListItem(req.params.id, req.body.item);
-    if (!shoppingList) {
-        res.status(400).json({ message: 'List not found.' });
-    } else if (!listItem) {
-        res.status(400).json({ message: 'Item not found' });
-    } else {
-        let item = makeStringToItem(req.body.item);
-        Object.assign(listItem, item);
-        await shoppingList.save();
-        res.status(200).json(shoppingList);
-    }
-};
-
-// put('/update/list/additem/:id')
-const addItemsToList = async (req: Request, res: Response) => {
-    let errorMsg = checkProperties(req, ["items"]);
-    if (errorMsg) {
-        res.status(401).json({ 'message': errorMsg });
-        return;
-    }
-
-    let shoppingList = await ShoppingList.findById({ _id: req.params.id });
-    let newItems = req.body.items.map(makeStringToItem);
-
-    shoppingList.items.push(...newItems);
-    await shoppingList.save();
-
-    res.status(200).json(shoppingList);
-};
-
-// patch('/update/list/togglebought/:id')
 const toggleItemAsBought = async (req: Request, res: Response) => {
-    let errorMsg = checkProperties(req, ["item"]);
-    if (errorMsg) {
-        res.status(401).json({ 'message': errorMsg });
-        return;
+    const { listID, itemID } = req.params;
+    const { shoppingList, listItem } = await getListItem(listID, itemID);
+    try {
+        if (!shoppingList) {
+            throw new ListNotFoundError(`Could not find list with ID: ${listID}`);
+        } else if (!listItem) {
+            throw new ListItemNotFoundError(`Could not find item with ID: ${itemID}`);
+        } else {
+            listItem.bought = !listItem.bought;
+            shoppingList.save();
+            res.status(200).json({ list: shoppingList });;
+        }
+    } catch (ex) {
+        if (ex instanceof ListNotFoundError) {
+            res.status(400).json({ message: ex.message });
+        } else if (ex instanceof ListItemNotFoundError) {
+            res.status(400).json({ message: ex.message });
+        } else {
+            logging.error(workspace, "Could not update item.", ex.message);
+            res.status(500);
+        }
     }
-    let { shoppingList, listItem } = await getListItem(req.params.id, req.body.item);
-    if (!shoppingList) {
-        res.status(400).json({ message: 'List not found.' });
-    } else if (!listItem) {
-        res.status(400).json({ message: 'Item not found.' });
-    } else {
-        listItem.bought = !listItem.bought;
-        shoppingList.markModified('items');
-        await shoppingList.save();
-        res.status(200).json({ shoppingList });
+};
+
+const createList = async (req: Request, res: Response) => {
+    const items = req.body.items.map(makeStringToItem);
+    const shoppingList: IShoppingList = { name: req.body.name, items: items, hidden: false };
+    try {
+        const newList = new ShoppingList(shoppingList);
+        await newList.save();
+        res.status(201).json({ 'list': newList });
+    } catch (ex) {
+        if (ex instanceof mongoose.Error.ValidationError) {
+            res.status(400).json({ message: 'Malformed request.' });
+        } else {
+            res.status(500);
+        }
     }
 };
 
